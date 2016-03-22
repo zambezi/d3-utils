4de1126e7bbb15d65ab487d139f63fd082cd0749
Peter Galiba <Peter.Galiba@rbs.com>
2016-03-22 12:19:37 +0000

    Adding tests and test dependencies

---
+++ test/test-main.js
@@ -0,0 +1,35 @@
+var keys = Object.keys(window.__karma__.files)
+  , isTest = /\.test\.js$/
+  , tests = keys.filter(onlyTests)
+
+requirejs.config(
+  {
+      baseUrl: '/base'
+    , paths: {
+        'fun': 'node_modules/fun'
+      , 'Squire': 'node_modules/squirejs/src/Squire'
+      , 'd3': 'node_modules/d3/d3'
+      , 'd3-utils': 'd3-utils'
+      , 'jquery': 'node_modules/jquery/dist/jquery'
+      , 'sinon': 'node_modules/karma-sinon-chai/node_modules/sinon/lib/sinon'
+      , 'text': 'node_modules/text/text'
+      , 'underscore': 'node_modules/underscore/underscore'
+    }
+    , shim : {
+        d3: { exports: 'd3' }
+      , sinon: { exports: 'sinon' }
+    }
+  }
+)
+
+require(
+    ['jquery']
+  , function($) {
+    window.Sizzle = $.find // Expose Sizzle to D3
+    require(tests, window.__karma__.start)
+  }
+)
+
+function onlyTests(file) {
+  return isTest.test(file)
+}
