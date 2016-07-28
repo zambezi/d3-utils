7bb28cb428ff5c1734c37beb5c461f7268706cd0
Matthew Dunsdon <Matthew.Dunsdon@rbs.com>
2016-07-28 14:21:24 +0100

    set up build using rollup and babel

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel'
+import babelrc from 'babelrc-rollup'
+
+export default {
+  entry: 'src/index.js',
+  dest: 'dist/d3-utils.js',
+  format: 'umd',
+  moduleName: 'simpleTable',
+  external: [ 'underscore' ],
+  sourceMap: true,
+  plugins: [ babel(babelrc()) ],
+  globals: {
+    'underscore': '_'
+  }
+}
