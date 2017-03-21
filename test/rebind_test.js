cf1a7ff3b3fafba0cfc91cc5247a462b50ee4f22
Gabriel Montagné <Gabriel.Montagne@icap.com>
2017-03-21 17:49:58 +0100

    Setup standard configuration.

--- test/rebind_test.js
+++ test/rebind_test.js
@@ -2,10 +2,9 @@ import { rebind } from '../src'
 import { deepEqual, strictEqual } from 'assert'
 
 describe('rebind', () => {
-
-  it('source function always has source as context', ()=> {
+  it('source function always has source as context', () => {
     const target = {}
-        , source = { method: function() { that = this }}
+    const source = { method }
 
     let that
 
@@ -14,12 +13,14 @@ describe('rebind', () => {
     strictEqual((target.method(), that), source)
     strictEqual((target.method.call({}), that), source)
 
+    function method () {
+      that = this
+    }
   })
 
   it("source function receives target function's arguments", () => {
-
     const target = {}
-        , source = { method: function() { those = Array.prototype.slice.call(arguments) }}
+    const source = { method }
 
     let those
 
@@ -29,59 +30,58 @@ describe('rebind', () => {
     deepEqual((target.method(null), those), [null])
     deepEqual((target.method(source, source, 1), those), [source, source, 1])
 
+    function method () {
+      those = Array.prototype.slice.call(arguments)
+    }
   })
 
-  it('target function returns target if source function returns source', ()=> {
-
+  it('target function returns target if source function returns source', () => {
     const target = {}
-        , source = { method: (value) => value ? source : 42 }
+    const source = { method: (value) => value ? source : 42 }
 
     rebind().from(source, 'method')(target)
 
     strictEqual(target.method(true), target)
   })
-  it('otherwise, target function returns source function return value', ()=> {
-    const target = {}, source = { method: function(value) { return value ? source : 42 } }
+  it('otherwise, target function returns source function return value', () => {
+    const target = {}
+    const source = { method: function (value) { return value ? source : 42 } }
     rebind().from(source, 'method')(target)
     strictEqual(target.method(), 42)
   })
 
-  it('can bind multiple methods', ()=> {
-    const target = {}, source = {
-            foo: function() { return 1 },
-            bar: function() { return 2 }
-          }
+  it('can bind multiple methods', () => {
+    const target = {}
+    const source = {
+      foo: function () { return 1 },
+      bar: function () { return 2 }
+    }
 
     rebind().from(source, 'foo', 'bar')(target)
     strictEqual(target.foo(), 1)
     strictEqual(target.bar(), 2)
   })
 
-  it('returns the target object', ()=> {
-
+  it('returns the target object', () => {
     const target = {}
-        , source = { foo: function() {} }
+    const source = { foo: function () {} }
 
     strictEqual(rebind().from(source, 'foo')(target), target)
   })
 
-  it('can bind methods from multiple sources', ()=> {
-
+  it('can bind methods from multiple sources', () => {
     const target = {}
-        , sourceA= { foo: (value) => value ? sourceA : 42 }
-        , sourceB= { bar: (value) => value ? sourceB : 'red' }
-        , sourceC= { yeh: (value) => value ? sourceC : 24 }
+    const sourceA = { foo: (value) => value ? sourceA : 42 }
+    const sourceB = { bar: (value) => value ? sourceB : 'red' }
+    const sourceC = { yeh: (value) => value ? sourceC : 24 }
 
     rebind()
         .from(sourceA, 'foo')
         .from(sourceB, 'bar')
-        .from(sourceC, 'yeh')
-        (target)
+        .from(sourceC, 'yeh')(target)
 
     strictEqual(target.foo(), 42)
     strictEqual(target.bar(), 'red')
     strictEqual(target.yeh(), 24)
-
   })
-
 })
